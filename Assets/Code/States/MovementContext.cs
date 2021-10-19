3e024b26d48bc4e2085b00b3510292f2b98238bf
Дон <dsvinkabot@gmail.com>
2021-10-19 21:37:10 +0300

    Добавлены состояния для движений игрока

---
+++ Assets/Code/States/MovementContext.cs
@@ -0,0 +1,40 @@
+﻿using Code.Models;
+using UnityEngine;
+
+namespace Code.States
+{
+    internal sealed class MovementContext
+    {
+        private MovementState _movementState;
+
+        public MovementContext(MovementState movementState, PlayerModel player)
+        {
+            _movementState = movementState;
+            Player = player;
+        }
+
+        public MovementState MovementState { set => _movementState = value; }
+        
+        public float DeltaTime { get; private set; }
+        public bool RunInput { get; private set; }
+        public bool JumpInput { get; private set; }
+        
+        public Vector3 MovementInput { get; private set; }
+        public Vector3 MovementDirection { get; set; }
+
+        public PlayerModel Player { get; }
+
+        public void Update(float deltaTime, Vector3 movementInput, bool runInput, bool jumpInput)
+        {
+            RunInput = runInput;
+            JumpInput = jumpInput;
+            MovementInput = movementInput;
+            DeltaTime = deltaTime;
+        }
+
+        public void Request()
+        {
+            _movementState.Handle(this);
+        }
+    }
+}
---
+++ Assets/Code/States/Player/MovementWalk.cs
@@ -0,0 +1,47 @@
+﻿using Code.Models;
+using UnityEngine;
+
+namespace Code.States.Player
+{
+    internal sealed class MovementWalk: MovementState
+    {
+        private MovementState _runState;
+        private MovementState _jumpState;
+
+        public override void Handle(MovementContext context)
+        {
+            var characterController = context.Player.CharacterController;
+            var transform = context.Player.Transform;
+            var canMove = context.Player.CanMove;
+            var data = context.Player.Data;
+        
+            var forward = transform.TransformDirection(Vector3.forward);
+            var right = transform.TransformDirection(Vector3.right);
+        
+            var curSpeedX = canMove ? data.WalkingSpeed * context.MovementInput.y : 0;
+            var curSpeedY = canMove ? data.WalkingSpeed * context.MovementInput.x : 0;
+            
+            var direction = (forward * curSpeedX) + (right * curSpeedY);
+            direction.y = context.MovementDirection.y;
+
+            characterController.Move(direction * context.DeltaTime);
+            context.MovementDirection = direction;
+            
+            if (context.RunInput)
+            {
+                if (_runState == null)
+                    _runState = new MovementRun();
+            
+                context.MovementState = _runState;
+            }
+            
+            else if (context.JumpInput)
+            {
+                if (_jumpState == null)
+                    _jumpState = new MovementJump();
+            
+                context.MovementState = _jumpState;
+            }
+        }
+    }
+}
